20d3084c21c9a3c35d242e2b34ddc8b88b5f2f77
pooya dehghan <pooyadehghan@yahoo.com>
2020-09-02 12:43:15 -0700

    work
    yeah no idea whats gonna happen

---
+++ precache-manifest.2e2a6d49425fbb4d8684794a2bc0aa84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8691091bc5f076db048a1cef676bd963",
+    "url": "/index.html"
+  },
+  {
+    "revision": "377ee4c62c8323148115",
+    "url": "/static/css/main.9ede3a5b.chunk.css"
+  },
+  {
+    "revision": "350ee1560c5fa6b2d598",
+    "url": "/static/js/2.048e371e.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/static/js/2.048e371e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377ee4c62c8323148115",
+    "url": "/static/js/main.f2fcc63c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8752958b16256b52441d89086db87382",
+    "url": "/static/media/avatar.8752958b.jpg"
+  },
+  {
+    "revision": "7ec6b22d1a4baa7ec8fc0138b94fa9f3",
+    "url": "/static/media/background.7ec6b22d.jpg"
+  }
+]);
